ea87f20601e5fd0a49102d208f1d66b7fe0b7b67
norfatiah <60971441+norfatiah@users.noreply.github.com>
2020-05-20 12:58:32 +0800

    Add files via upload

---
+++ lib/mainscreen.dart
@@ -0,0 +1,390 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:progress_dialog/progress_dialog.dart';
+import 'package:toast/toast.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+
+
+class MainScreen extends StatefulWidget {
+  
+
+  
+
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  List loadData;
+  int curnumber = 1;
+  double screenHeight, screenWidth;
+  bool _visible = false;
+  String curtype = "Recent";
+  String titlecenter = "No location found";
+ 
+ 
+
+  @override
+  void initState() {
+    super.initState();
+    _loadData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    screenHeight = MediaQuery.of(context).size.height;
+    screenWidth = MediaQuery.of(context).size.width;
+
+    return MaterialApp(
+        title: 'Material App',
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text(
+              'Load Destination List',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            actions: <Widget>[
+              IconButton(
+                icon: _visible
+                    ? new Icon(Icons.expand_more)
+                    : new Icon(Icons.expand_less),
+                onPressed: () {
+                  setState(() {
+                    if (_visible) {
+                      _visible = false;
+                    } else {
+                      _visible = true;
+                    }
+                  });
+                },
+              ),
+
+              //
+            ],
+          ),
+          body: Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Visibility(
+                  visible: _visible,
+                      child: Padding(
+                          padding: EdgeInsets.all(5),
+                          child: SingleChildScrollView(
+                            scrollDirection: Axis.horizontal,
+                            child: Row(
+                              children: <Widget>[
+                                Column(
+                                  children: <Widget>[
+                                    
+
+                                    FlatButton(
+                                        onPressed: () => _sortDestination("JOHOR"),
+                                        color:
+                                            Color.fromRGBO(101, 255, 218, 50),
+                                        padding: EdgeInsets.all(10.0),
+                                        child: Column(
+                                          // Replace with a Row for horizontal icon + text
+                                          children: <Widget>[
+                                            Text(
+                                              "JOHOR",
+                                              style: TextStyle(
+                                                  color: Colors.black),
+                                            )
+                                          ],
+                                        )),
+                                  ],
+                                ),
+                                SizedBox(
+                                  width: 3,
+                                ),
+                                Column(
+                                  children: <Widget>[
+                                    FlatButton(
+                                        onPressed: () => _sortDestination("KELANTAN"),
+                                        color:
+                                            Color.fromRGBO(101, 255, 218, 50),
+                                        padding: EdgeInsets.all(10.0),
+                                        child: Column(
+                                          // Replace with a Row for horizontal icon + text
+                                          children: <Widget>[
+                                            Text(
+                                              "KELANTAN",
+                                              style: TextStyle(
+                                                  color: Colors.black),
+                                            )
+                                          ],
+                                        )),
+                                  ],
+                                ),
+                                SizedBox(
+                                  width: 3,
+                                ),
+                                Column(
+                                  children: <Widget>[
+                                    FlatButton(
+                                        onPressed: () =>
+                                            _sortDestination("TERENGGANU"),
+                                        color:
+                                            Color.fromRGBO(101, 255, 218, 50),
+                                        padding: EdgeInsets.all(10.0),
+                                        child: Column(
+                                          // Replace with a Row for horizontal icon + text
+                                          children: <Widget>[
+                                            Text(
+                                              "TERENGGANU",
+                                              style: TextStyle(
+                                                  color: Colors.black),
+                                            )
+                                          ],
+                                        )),
+                                  ],
+                                ),
+                                SizedBox(
+                                  width: 3,
+                                ),
+                                Column(
+                                  children: <Widget>[
+                                    FlatButton(
+                                        onPressed: () => _sortDestination("SELANGOR"),
+                                        color:
+                                            Color.fromRGBO(101, 255, 218, 50),
+                                        padding: EdgeInsets.all(10.0),
+                                        child: Column(
+                                          // Replace with a Row for horizontal icon + text
+                                          children: <Widget>[
+                                            Text(
+                                              "SELANGOR",
+                                              style: TextStyle(
+                                                  color: Colors.black),
+                                            )
+                                          ],
+                                        )),
+                                  ],
+                                ),
+                                SizedBox(
+                                  width: 3,
+                                ),
+                                Column(
+                                  children: <Widget>[
+                                    FlatButton(
+                                        onPressed: () => _sortDestination("PAHANG"),
+                                        color:
+                                            Color.fromRGBO(101, 255, 218, 50),
+                                        padding: EdgeInsets.all(10.0),
+                                        child: Column(
+                                          // Replace with a Row for horizontal icon + text
+                                          children: <Widget>[                                          
+                                            Text(
+                                              "PAHANG",
+                                              style: TextStyle(
+                                                  color: Colors.black),
+                                            )
+                                          ],
+                                        )),
+                                  ],
+                                ),
+                                SizedBox(
+                                  width: 3,
+                                ),
+                                Column(
+                                  children: <Widget>[
+                                    FlatButton(
+                                        onPressed: () => _sortDestination("NEGERI SEMBILAN"),
+                                        color:
+                                            Color.fromRGBO(101, 255, 218, 50),
+                                        padding: EdgeInsets.all(10.0),
+                                        child: Column(
+                                          // Replace with a Row for horizontal icon + text
+                                          children: <Widget>[
+                                            Text(
+                                              "NEGERI SEMBILAN",
+                                              style: TextStyle(
+                                                  color: Colors.black),
+                                            )
+                                          ],
+                                        )),
+                                  ],
+                                ),
+                                SizedBox(
+                                  width: 3,
+                                ),
+                                Column(
+                                  children: <Widget>[
+                                    FlatButton(
+                                        onPressed: () => _sortDestination("PERAK"),
+                                        color:
+                                            Color.fromRGBO(101, 255, 218, 50),
+                                        padding: EdgeInsets.all(10.0),
+                                        child: Column(
+                                          // Replace with a Row for horizontal icon + text
+                                          children: <Widget>[                                     
+                                            Text(
+                                              "PERAK",
+                                              style: TextStyle(
+                                                  color: Colors.black),
+                                            )
+                                          ],
+                                        )),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ))),
+                
+               
+                Text(curtype,
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white)),
+                loadData == null
+                    ? Flexible(
+                        child: Container(
+                            child: Center(
+                                child: Text(
+                        titlecenter,
+                        style: TextStyle(
+                            color: Color.fromRGBO(101, 255, 218, 50),
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold),
+                      ))))
+                    : Expanded(
+                        child: GridView.count(
+                            crossAxisCount: 2,
+                            childAspectRatio:
+                                (screenWidth / screenHeight) / 0.8,
+                            children:
+                                List.generate(loadData.length, (index) {
+                              return Container(
+                                  child: Card(
+                                      elevation: 10,
+                                      child: Padding(
+                                        padding: EdgeInsets.all(5),
+                                        child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: <Widget>[
+                                            GestureDetector(
+                                              onTap: () =>
+                                                  _onImageDisplay(index),
+                                              child: Container(
+                                                height: screenHeight / 5.9,
+                                                width: screenWidth / 3.5,
+                                                child: ClipOval(
+                                                    child: CachedNetworkImage(
+                                                  fit: BoxFit.fill,
+                                                  imageUrl:
+                                                      "http://slumberjer.com/visitmalaysia/images/${loadData[index]['id']}.jpg",
+                                                  placeholder: (context, url) =>
+                                                      new CircularProgressIndicator(),
+                                                  errorWidget:
+                                                      (context, url, error) =>
+                                                          new Icon(Icons.error),
+                                                )),
+                                              ),
+                                            ),
+                                            Text(loadData[index]['Location name'],
+                                                maxLines: 1,
+                                                style: TextStyle(
+                                                    fontWeight: FontWeight.bold,
+                                                    color: Colors.white)),
+                                           
+                                            Text(loadData[index]['State'],
+                                                maxLines: 1,
+                                                style: TextStyle(
+                                                    fontWeight: FontWeight.bold,
+                                                    color: Colors.white)),
+                                
+                                          ],
+                                        ),
+                                      )));
+                            })))
+              ],
+            ),
+          ),
+      
+        ));
+  }
+
+  _onImageDisplay(int index) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+            backgroundColor: Colors.white,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(20.0))),
+            content: new Container(
+              color: Colors.white,
+              height: screenHeight / 2.2,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                      height: screenWidth / 1.5,
+                      width: screenWidth / 1.5,
+                      decoration: BoxDecoration(
+                          //border: Border.all(color: Colors.black),
+                          image: DecorationImage(
+                              fit: BoxFit.scaleDown,
+                              image: NetworkImage(
+                                  "http://slumberjer.com/visitmalaysia/images/${loadData[index]['id']}.jpg")))),
+                ],
+              ),
+            ));
+      },
+    );
+  }
+
+  void _loadData() {
+    String urlLoadJobs = "http://slumberjer.com/visitmalaysia/load_destinations.php";
+    http.post(urlLoadJobs, body: {}).then((res) {
+      setState(() {
+        var extractdata = json.decode(res.body);
+        loadData = extractdata["Destination"];
+      });
+    }).catchError((err) {
+      print(err);
+    });
+  }
+
+  
+
+  void _sortDestination(String type) {
+    try {
+      ProgressDialog pr = new ProgressDialog(context,
+          type: ProgressDialogType.Normal, isDismissible: true);
+      pr.style(message: "Searching...");
+      pr.show();
+      String urlLoadJobs =
+          "http://slumberjer.com/visitmalaysia/load_destinations.php";
+      http.post(urlLoadJobs, body: {
+        "type": type,
+      }).then((res) {
+        setState(() {
+          curtype = type;
+          var extractdata = json.decode(res.body);
+          loadData = extractdata["Destination"];
+          FocusScope.of(context).requestFocus(new FocusNode());
+          pr.dismiss();
+        });
+      }).catchError((err) {
+        print(err);
+        pr.dismiss();
+      });
+      pr.dismiss();
+    } catch (e) {
+      Toast.show("Error", context,
+          duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
+    }
+  }
+
+  
+
+}
---
+++ lib/splashscreen.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:visitmalaysia/mainscreen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  
+  double screenHeight;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    theme: new ThemeData(
+primarySwatch: Colors.red),
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+       backgroundColor: Color(0xFFF8BBD0),
+       body: Center( 
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+             Image.asset(
+                  'assets/images/pic1.jpg',
+                   width: 230,
+                   height: 230,
+             ),
+             SizedBox(
+                   height: 20,
+            ),
+          new ProgressIndicator(),
+          
+    ],
+   ),
+  ),
+ ),
+);
+
+  }
+}
+
+class ProgressIndicator extends StatefulWidget {
+  @override
+  _ProgressIndicatorState createState() => new _ProgressIndicatorState();
+}
+
+class _ProgressIndicatorState extends State<ProgressIndicator>
+  with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation<double> animation;
+
+
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+        duration: const Duration(milliseconds: 2000), vsync: this);
+    animation = Tween(begin: 0.0, end: 1.0).animate(controller)
+      ..addListener(() {
+        setState(() {
+          //updating states
+          if (animation.value > 0.99) {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (BuildContext context) => MainScreen()));
+          }
+        });
+      });
+    controller.repeat();
+  }
+
+  @override
+  void dispose() {
+    controller.stop();
+    super.dispose();
+  }
+
+
+@override
+  Widget build(BuildContext context) {
+    return new Center(
+      child: new Container(
+      //width: 300,
+      child: CircularProgressIndicator(
+        value: animation.value,
+        //backgroundColor: Colors.brown,
+        valueColor: new AlwaysStoppedAnimation<Color>(Colors.red,
+      
+    )
+      )
+    ));
+  }
+
+
+}
